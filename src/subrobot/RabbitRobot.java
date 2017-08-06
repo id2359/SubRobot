32e087c02fabde7163957a4b58bdec7e5b1de44c
l___r <l___r@LAPTOP-M7D13UD7>
2017-08-06 13:25:47 +0800

    Make base robot an abstraction over sensors

--- src/subrobot/RabbitRobot.java
+++ src/subrobot/RabbitRobot.java
@@ -1,7 +1,18 @@
 package subrobot;
-
+import lejos.nxt.Button;
+import lejos.nxt.Motor;
+import lejos.nxt.NXTRegulatedMotor;
 
 public class RabbitRobot {
+	NXTRegulatedMotor leftLeg;
+	NXTRegulatedMotor rightLeg;
+	Button nose;
+	
+	RabbitRobot() {
+		leftLeg = Motor.A;
+		rightLeg = Motor.C;
+	}
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
