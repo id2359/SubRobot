1159bf6617534b70d12f9f1f0728df1b3c58317f
l___r <l___r@LAPTOP-M7D13UD7>
2017-08-06 13:00:03 +0800

    First commit

---
+++ src/subrobot/AvoidWallBehaviour.java
@@ -0,0 +1,24 @@
+package subrobot;
+import lejos.robotics.subsumption.Behavior;
+public class AvoidWallBehaviour implements Behavior {
+
+	@Override
+	public boolean takeControl() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void action() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void suppress() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
+
---
+++ src/subrobot/RabbitRobot.java
@@ -0,0 +1,11 @@
+package subrobot;
+
+
+public class RabbitRobot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
